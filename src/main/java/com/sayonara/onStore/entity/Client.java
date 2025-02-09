f60873998f1505f1c83dff635eca26062005b253
Sayonara <sayonaraf@mail.ru>
2025-02-09 10:35:52 +0300

    02: update application.properties with Hibernate settings

--- src/main/java/com/sayonara/onStore/entity/Client.java
+++ src/main/java/com/sayonara/onStore/entity/Client.java
@@ -12,23 +12,15 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String name;
-
     private String surname;
-
     private String patronymic;
-
     private char gender;
-
     @Column(name = "birth_date")
     private LocalDate dateOfBirth;
-
     @Transient
     private int age;
-
     private String email;
-
     private String phone;
 
     public int getAge() {
